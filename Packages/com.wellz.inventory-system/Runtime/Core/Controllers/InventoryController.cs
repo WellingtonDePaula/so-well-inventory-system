c647726438bcd2cf8cb0fd85ee078399c2fb5802
Wellz <owellingtondepaula@gmail.com>
2026-06-28 15:15:37 -0300

    refactor(controllers): extrair InventoryControllerBase
    Extrai a lógica comum de InventoryController para uma nova classe base InventoryControllerBase. Campos serializáveis, grid e callbacks de ciclo de vida (Awake/OnEnable/OnDisable) foram movidos para a base; InventoryController agora herda e sobrescreve os handlers de input (HandleOnPressed/Released/Other/PositionChanged). Objetivo: reduzir duplicação e facilitar extensão/testes. Inclui também o arquivo .meta gerado.

--- Packages/com.wellz.inventory-system/Runtime/Core/Controllers/InventoryController.cs
+++ Packages/com.wellz.inventory-system/Runtime/Core/Controllers/InventoryController.cs
@@ -3,60 +3,20 @@ using Wellz.Inventory.Input;
 using Wellz.Utils.Core;
 
 namespace Wellz.Inventory.Core.Controllers {
-    public class InventoryController : MonoBehaviour {
+    public class InventoryController : InventoryControllerBase {
         // Campos estáticos e constantes
 
         // Campos expostos no Inspector
-        [SerializeField] private GameObject slotPrefab;
-        [SerializeField] int width;
-        [SerializeField] int height;
-        [SerializeField] Transform slotsTransform;
-
-        [SerializeField] InputProvider inputProvider;
 
         // Propriedades para acesso controlado externo
 
         // Campos privados para o estado interno da classe
-        private SlotController currentHoverSlot = null;
-        private SlotController currentSelectedSlot = null;
-        private GenericGrid<SlotController> inventoryGrid;
 
         #region Métodos do ciclo de vida da Unity (Awake, OnEnable, Start, OnDisable)
-        private void Awake() {
-            inventoryGrid = new GenericGrid<SlotController>(width, height, 1, default, (grid, x, y) => {
-                var slot = Instantiate(slotPrefab, slotsTransform).GetComponent<SlotController>();
-                slot.transform.localPosition = new Vector3(x, y, 0);
-                return slot;
-            });
-        }
-
-        private void OnEnable() {
-            if (inputProvider != null) {
-                inputProvider.Activate();
-                inputProvider.OnPressed += HandleOnPressed;
-                inputProvider.OnReleased += HandleOnReleased;
-
-                inputProvider.OnOtherPressed += HandleOnOtherPressed;
-                inputProvider.OnOtherReleased += HandleOnOtherReleased;
-                inputProvider.OnPositionChanged += HandleOnPositionChanged;
-            }
-        }
-
-        private void OnDisable() {
-            if (inputProvider != null) {
-                inputProvider.OnPressed -= HandleOnPressed;
-                inputProvider.OnReleased -= HandleOnReleased;
-
-                inputProvider.OnOtherPressed -= HandleOnOtherPressed;
-                inputProvider.OnOtherReleased -= HandleOnOtherReleased;
-                inputProvider.OnPositionChanged -= HandleOnPositionChanged;
-                inputProvider.Deactivate();
-            }
-        }
         #endregion
 
         #region Métodos públicos e privados da lógica da classe
-        private void HandleOnPressed() {
+        protected override void HandleOnPressed() {
             if (currentHoverSlot == null) {
                 return;
             }
@@ -71,18 +31,18 @@ namespace Wellz.Inventory.Core.Controllers {
                 currentSelectedSlot = currentHoverSlot;
             }
         }
-        private void HandleOnReleased() {
+        protected override void HandleOnReleased() {
             //throw new NotImplementedException();
         }
-        private void HandleOnOtherReleased() {
+        protected override void HandleOnOtherReleased() {
             //throw new NotImplementedException();
         }
 
-        private void HandleOnOtherPressed() {
+        protected override void HandleOnOtherPressed() {
             if (currentSelectedSlot == null) { return; }
             currentSelectedSlot.AddItem(currentSelectedSlot.Item, 1);
         }
-        private void HandleOnPositionChanged(Vector2 pos) {
+        protected override void HandleOnPositionChanged(Vector2 pos) {
             SlotController slotUnder = null;
             inventoryGrid.ForEach((x, y, slot) => {
                 if (RectTransformUtility.RectangleContainsScreenPoint(slot.RectTransform, pos)) {
---
+++ Packages/com.wellz.inventory-system/Runtime/Core/Controllers/InventoryControllerBase.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using Wellz.Inventory.Input;
+using Wellz.Utils.Core;
+
+namespace Wellz.Inventory.Core.Controllers {
+    public abstract class InventoryControllerBase : MonoBehaviour {
+        // Campos estáticos e constantes
+
+        // Campos expostos no Inspector
+        [SerializeField] protected GameObject slotPrefab;
+        [SerializeField] protected int width;
+        [SerializeField] protected int height;
+        [SerializeField] protected Transform slotsTransform;
+
+        [SerializeField] protected InputProvider inputProvider;
+
+        // Propriedades para acesso controlado externo
+
+        // Campos privados para o estado interno da classe
+        protected SlotController currentHoverSlot = null;
+        protected SlotController currentSelectedSlot = null;
+        protected GenericGrid<SlotController> inventoryGrid;
+
+        #region Métodos do ciclo de vida da Unity (Awake, OnEnable, Start, OnDisable)
+        protected virtual void Awake() {
+            inventoryGrid = new GenericGrid<SlotController>(width, height, 1, default, (grid, x, y) => {
+                var slot = Instantiate(slotPrefab, slotsTransform).GetComponent<SlotController>();
+                slot.transform.localPosition = new Vector3(x, y, 0);
+                return slot;
+            });
+        }
+
+        protected virtual void OnEnable() {
+            if (inputProvider != null) {
+                inputProvider.Activate();
+                inputProvider.OnPressed += HandleOnPressed;
+                inputProvider.OnReleased += HandleOnReleased;
+
+                inputProvider.OnOtherPressed += HandleOnOtherPressed;
+                inputProvider.OnOtherReleased += HandleOnOtherReleased;
+
+                inputProvider.OnPositionChanged += HandleOnPositionChanged;
+            }
+        }
+
+        protected virtual void OnDisable() {
+            if (inputProvider != null) {
+                inputProvider.OnPressed -= HandleOnPressed;
+                inputProvider.OnReleased -= HandleOnReleased;
+
+                inputProvider.OnOtherPressed -= HandleOnOtherPressed;
+                inputProvider.OnOtherReleased -= HandleOnOtherReleased;
+
+                inputProvider.OnPositionChanged -= HandleOnPositionChanged;
+                inputProvider.Deactivate();
+            }
+        }
+        #endregion
+
+        #region Métodos públicos e privados da lógica da classe
+        protected abstract void HandleOnPressed();
+        protected abstract void HandleOnReleased();
+
+        protected abstract void HandleOnOtherReleased();
+        protected abstract void HandleOnOtherPressed();
+
+        protected abstract void HandleOnPositionChanged(Vector2 pos);
+    }
+        #endregion
+}
